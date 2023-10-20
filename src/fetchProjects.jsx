0cab65a0fed824bf365558c9a3f98c73d48732b1
Lawrence Caulker <locaulker@gmail.com>
2023-10-20 06:35:08 -0400

    implemented useFetchProjects custom hook

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react"
 import { createClient } from "contentful"
 
 const client = createClient({
@@ -6,6 +7,29 @@ const client = createClient({
   accessToken: "EbiKMShE6QNaKssF8KH5oUYdMnF2O1G_TvP8WOVJ-_s"
 })
 
-client
-  .getEntries({ content_type: "projects" })
-  .then((response) => console.log(response))
+export const useFetchProjects = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [projects, setProjects] = useState([])
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: "projects" })
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields
+        const id = item.sys.id
+        const img = item?.fields?.file?.url
+        return { title, url, id, img }
+      })
+      setProjects(projects)
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+  return { isLoading, projects }
+}
