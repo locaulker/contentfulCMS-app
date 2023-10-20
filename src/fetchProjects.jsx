be125a23b6c159b425cc854906ee81fc24069067
Lawrence Caulker <locaulker@gmail.com>
2023-10-20 07:07:51 -0400

    update custom hook

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -2,9 +2,7 @@ import { useState, useEffect } from "react"
 import { createClient } from "contentful"
 
 const client = createClient({
-  space: "uyniivyx5mup",
-  environment: "master",
-  accessToken: "EbiKMShE6QNaKssF8KH5oUYdMnF2O1G_TvP8WOVJ-_s"
+  //
 })
 
 export const useFetchProjects = () => {
